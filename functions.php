e503d6e0bf705809e434ff1dd148e398594efe08
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-10-20 21:10:21 +1100

    Got it kinda working

--- functions.php
+++ functions.php
@@ -55,29 +55,8 @@ if ( ! function_exists( 'darkpastel_styles' ) ) {
 
 add_action( 'wp_enqueue_scripts', 'darkpastel_styles' );
 
-if ( ! function_exists( 'darkpastel_register_block_styles' ) ) {
-	/**
-	 * Register theme specific block styles.
-	 * 
-	 * @since Dark Pastel 1.0
-	 * 
-	 * @return void
-	 */
-	// TODO: Try to output the SVG to the site frontend, as it happens for Duotone.
-	// Basically, similar to this: https://github.com/wordpress/gutenberg/blob/739f26f6814ba1a32576af5ae5f4c87206d55ff4/lib/block-supports/duotone.php#L354
-	function darkpastel_register_block_styles() {
-		register_block_style(
-			'core/separator',
-			array(
-				'name'         => 'double-ornament',
-				'label'        => __( 'Double Ornament', 'darkpastel' ),
-				'inline_style' => '.wp-block-separator.is-style-double-ornament { width: 200px !important; height: 20px; background-color: green; clip-path: url(' . esc_url( get_template_directory_uri() ) . '/assets/images/test-svg.svg#svgPath); }',
-			)
-		);
-	}
-}
-
-add_action( 'init', 'darkpastel_register_block_styles' );
+// Add block patterns
+require_once get_template_directory() . '/inc/block-styles.php';
 
 // Add block patterns
-require get_template_directory() . '/inc/block-patterns.php';
+require_once get_template_directory() . '/inc/block-patterns.php';
