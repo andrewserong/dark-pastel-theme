6fe9e6492b0c23349cca8dc67f934ce78c874a91
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-10-22 12:36:51 +1100

    Add cutout circles

--- inc/block-styles.php
+++ inc/block-styles.php
@@ -3,9 +3,9 @@
 if ( ! function_exists( 'darkpastel_register_block_styles' ) ) {
 	/**
 	 * Register theme specific block styles.
-	 * 
+	 *
 	 * @since Dark Pastel 1.0
-	 * 
+	 *
 	 * @return void
 	 */
 	function darkpastel_register_block_styles() {
@@ -22,6 +22,38 @@ if ( ! function_exists( 'darkpastel_register_block_styles' ) ) {
 			array(
 				'name'         => 'double-ornament',
 				'label'        => __( 'Double Ornament', 'darkpastel' ),
+			)
+		);
+
+		register_block_style(
+			'core/separator',
+			array(
+				'name'         => 'star-ornament',
+				'label'        => __( 'Star Ornament', 'darkpastel' ),
+			)
+		);		
+
+		register_block_style(
+			'core/separator',
+			array(
+				'name'         => 'double-ornament-inverted',
+				'label'        => __( 'Double Ornament (inverted)', 'darkpastel' ),
+			)
+		);		
+
+		register_block_style(
+			'core/separator',
+			array(
+				'name'         => 'cutout-circle-up',
+				'label'        => __( 'Cutout circle (up)', 'darkpastel' ),
+			)
+		);
+
+		register_block_style(
+			'core/separator',
+			array(
+				'name'         => 'cutout-circle-down',
+				'label'        => __( 'Cutout circle (down)', 'darkpastel' ),
 			)
 		);
 
