38c0767bc2b5db35b70139ac5128dff5bb00796b
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-10-30 17:50:49 +1100

    Update 404 page

--- inc/patterns/hidden-404.php
+++ inc/patterns/hidden-404.php
@@ -5,11 +5,39 @@
 return array(
 	'title'    => __( '404 content', 'twentytwentytwo' ),
 	'inserter' => false,
-	'content'  => '<!-- wp:heading {"style":{"typography":{"fontSize":"clamp(4rem, 40vw, 20rem)","fontWeight":"200","lineHeight":"1"}},"className":"has-text-align-center"} -->
-					<h2 class="has-text-align-center" style="font-size:clamp(4rem, 40vw, 20rem);font-weight:200;line-height:1">' . esc_html( _x( '404', 'Error code for a webpage that is not found.', 'twentytwentytwo' ) ) . '</h2>
+	'content'  => '<!-- wp:group {"layout":{"type":"default"}} -->
+					<div class="wp-block-group"><!-- wp:separator {"style":{"spacing":{"margin":{"bottom":"0"}}},"backgroundColor":"primary","className":"is-style-cutout-circle-down"} -->
+					<hr class="wp-block-separator has-text-color has-primary-color has-alpha-channel-opacity has-primary-background-color has-background is-style-cutout-circle-down" style="margin-bottom:0"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}},"backgroundColor":"primary","textColor":"background","layout":{"type":"constrained"}} -->
+					<div class="wp-block-group has-background-color has-primary-background-color has-text-color has-background" style="margin-top:0">
+
+					<!-- wp:spacer {"height":"1rem"} -->
+					<div style="height:1rem" aria-hidden="true" class="wp-block-spacer"></div>
+					<!-- /wp:spacer -->
+
+					<!-- wp:heading {"textAlign":"center"} -->
+					<h2 class="has-text-align-center">' . esc_html( _x( 'Alas, this is not a real page.', 'Heading for a webpage that is not found.', 'darkpastel' ) ) . '</h2>
 					<!-- /wp:heading -->
-					<!-- wp:paragraph {"align":"center"} -->
-					<p class="has-text-align-center">' . esc_html__( 'This page could not be found. Maybe try a search?', 'twentytwentytwo' ) . '</p>
+
+					<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}}} -->
+					<p class="has-text-align-center" style="margin-top:0">' . __( 'Perhaps it could be found via a search?', 'darkpastel' ) . '</p>
 					<!-- /wp:paragraph -->
-					<!-- wp:search {"label":"' . esc_html_x( 'Search', 'label', 'twentytwentytwo' ) . '","showLabel":false,"width":50,"widthUnit":"%","buttonText":"' . esc_html__( 'Search', 'twentytwentytwo' ) . '","buttonUseIcon":true,"align":"center"} /-->',
+
+					<!-- wp:search {"label":"' . esc_html_x( 'Search', 'label', 'darkpastel' ) . '","showLabel":false,"width":75,"widthUnit":"%","buttonText":"' . esc_html__( 'Search', 'darkpastel' ) . '","buttonUseIcon":true,"align":"center","style":{"border":{"width":"0px","style":"none"}},"backgroundColor":"background","textColor":"primary"} /-->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">' . esc_html( _x( '404', 'Error code for a webpage that is not found.', 'darkpastel' ) ) . '</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"backgroundColor":"background","className":"is-style-narrow-ornament"} -->
+					<hr class="wp-block-separator has-text-color has-background-color has-alpha-channel-opacity has-background-background-color has-background is-style-narrow-ornament"/>
+					<!-- /wp:separator --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","className":"is-style-cutout-circle-up"} -->
+					<hr class="wp-block-separator has-text-color has-primary-color has-alpha-channel-opacity has-primary-background-color has-background is-style-cutout-circle-up" style="margin-top:0;margin-bottom:0"/>
+					<!-- /wp:separator --></div>
+					<!-- /wp:group -->',
 );
