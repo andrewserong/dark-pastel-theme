66642b190abfb8bf83a3fbcb760953c67752c56a
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-11-20 16:59:17 +1100

    Update two column frame pattern

--- inc/patterns/dark-pastel-two-column-frame.php
+++ inc/patterns/dark-pastel-two-column-frame.php
@@ -10,8 +10,8 @@ return array(
 					<hr class="wp-block-separator has-text-color has-primary-color has-alpha-channel-opacity has-primary-background-color has-background is-style-cutout-circle-down" style="margin-bottom:0"/>
 					<!-- /wp:separator -->
 
-					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}},"backgroundColor":"primary","textColor":"background","layout":{"type":"constrained"}} -->
-					<div class="wp-block-group has-background-color has-primary-background-color has-text-color has-background" style="margin-top:0"><!-- wp:separator {"backgroundColor":"background","className":"is-style-double-ornament"} -->
+					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"primary","textColor":"background","layout":{"type":"constrained","contentSize":"32em"}} -->
+					<div class="wp-block-group has-background-color has-primary-background-color has-text-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:separator {"backgroundColor":"background","className":"is-style-double-ornament"} -->
 					<hr class="wp-block-separator has-text-color has-background-color has-alpha-channel-opacity has-background-background-color has-background is-style-double-ornament"/>
 					<!-- /wp:separator -->
 
