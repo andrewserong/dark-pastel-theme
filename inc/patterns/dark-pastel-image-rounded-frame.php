b46efbb78c158f99a2603218e05d4b207d2964b9
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-11-20 15:00:38 +1100

    Update image rounded frame pattern

--- inc/patterns/dark-pastel-image-rounded-frame.php
+++ inc/patterns/dark-pastel-image-rounded-frame.php
@@ -6,14 +6,12 @@ return array(
 	'title'      => __( 'Image rounded frame', 'darkpastel' ),
 	'categories' => array( 'darkpastel' ),
 	'content'    => '<!-- wp:group {"layout":{"type":"constrained"}} -->
-					<div class="wp-block-group">
-
-					<!-- wp:separator {"style":{"spacing":{"margin":{"bottom":"0"}}},"backgroundColor":"primary","className":"is-style-cutout-circle-down"} -->
+					<div class="wp-block-group"><!-- wp:separator {"style":{"spacing":{"margin":{"bottom":"0"}}},"backgroundColor":"primary","className":"is-style-cutout-circle-down"} -->
 					<hr class="wp-block-separator has-text-color has-primary-color has-alpha-channel-opacity has-primary-background-color has-background is-style-cutout-circle-down" style="margin-bottom:0"/>
 					<!-- /wp:separator -->
 
-					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}},"border":{"radius":"5rem"}},"backgroundColor":"primary","textColor":"background","layout":{"type":"constrained"}} -->
-					<div class="wp-block-group has-background-color has-primary-background-color has-text-color has-background" style="border-radius:5rem;margin-top:0"><!-- wp:separator {"backgroundColor":"background","className":"is-style-double-ornament"} -->
+					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"radius":"3rem"}},"backgroundColor":"primary","textColor":"background","layout":{"type":"constrained"}} -->
+					<div class="wp-block-group has-background-color has-primary-background-color has-text-color has-background" style="border-radius:3rem;margin-top:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:separator {"backgroundColor":"background","className":"is-style-double-ornament"} -->
 					<hr class="wp-block-separator has-text-color has-background-color has-alpha-channel-opacity has-background-background-color has-background is-style-double-ornament"/>
 					<!-- /wp:separator -->
 
@@ -28,8 +26,6 @@ return array(
 
 					<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0"}}},"backgroundColor":"primary","className":"is-style-cutout-circle-up"} -->
 					<hr class="wp-block-separator has-text-color has-primary-color has-alpha-channel-opacity has-primary-background-color has-background is-style-cutout-circle-up" style="margin-top:0"/>
-					<!-- /wp:separator -->
-
-					</div>
+					<!-- /wp:separator --></div>
 					<!-- /wp:group -->',
 );
