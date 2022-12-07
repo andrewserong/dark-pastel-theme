7f62e367912efd2c083487575ac57b38abf8e281
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-12-07 22:17:19 +1100

    Ensure remaining strings can be translated

---
+++ patterns/dark-pastel-hidden-recent-posts-heading.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Hidden Recent Posts Heading
+ * Slug: darkpastel/hidden-recent-posts-heading
+ * Inserter: no
+ */
+?>
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="has-text-align-center"><?php echo __( 'The most recent of posts…', 'darkpastel' ); ?></h2>
+<!-- /wp:heading -->
