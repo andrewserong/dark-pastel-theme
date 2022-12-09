c4128587808cb2929cc1709a21e4e796272bb8db
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-12-09 21:33:55 +1100

    Add script for packaging to a zip file

---
+++ package-dotorg.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# This script is borrowed from Automattic's theme repo. Based on:
+# https://github.com/Automattic/themes/blob/trunk/package-dotorg.sh
+
+mkdir dark-pastel
+rsync -avz --exclude dark-pastel --exclude-from './dotorg-exclude.txt' ./ dark-pastel
+rm -rf dark-pastel/dark-pastel
+zip -r -X dark-pastel.zip dark-pastel
+rm -rf dark-pastel
+git restore ./
